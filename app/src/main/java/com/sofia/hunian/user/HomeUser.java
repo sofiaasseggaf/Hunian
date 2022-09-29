e3a1b131ea52aae559dd324ba3f33f2faaeea5b9
sofiaasseggaf <sofiaasseggaf@gmail.com>
2022-09-30 03:03:36 +0700

    home user

--- app/src/main/java/com/sofia/hunian/user/HomeUser.java
+++ app/src/main/java/com/sofia/hunian/user/HomeUser.java
@@ -1,4 +1,4 @@
-package com.sofia.hunian.admin;
+package com.sofia.hunian.user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,21 +10,21 @@ import android.widget.TextView;
 
 import com.sofia.hunian.R;
 
-public class HomeAdmin extends AppCompatActivity {
+public class HomeUser extends AppCompatActivity {
 
     TextView txt_nama;
     HorizontalScrollView hsv_katalog;
-    ImageButton btn_laporan, btn_home, btn_katalog, btn_disukai, btn_profile;
+    ImageButton btn_keranjang, btn_home, btn_katalog, btn_disukai, btn_profile;
     LinearLayout btn_home2, btn_katalog2, btn_disukai2, btn_profile2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.admin_home);
+        setContentView(R.layout.user_home);
 
         txt_nama = findViewById(R.id.txt_nama);
         hsv_katalog = findViewById(R.id.hsv_katalog);
-        btn_laporan = findViewById(R.id.btn_laporan);
+        btn_keranjang = findViewById(R.id.btn_keranjang);
         btn_home = findViewById(R.id.btn_home);
         btn_katalog = findViewById(R.id.btn_katalog);
         btn_disukai = findViewById(R.id.btn_disukai);
@@ -33,6 +33,7 @@ public class HomeAdmin extends AppCompatActivity {
         btn_katalog2 = findViewById(R.id.btn_katalog2);
         btn_disukai2 = findViewById(R.id.btn_disukai2);
         btn_profile2 = findViewById(R.id.btn_profile2);
+
 
     }
 }
